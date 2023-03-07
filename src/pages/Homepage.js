fbdb52f4bd15d7cfb78fc19a41493e410d0ef36d
RahulMainwal <57298309+RahulMainwal@users.noreply.github.com>
2023-03-07 16:27:26 +0530

    Update Homepage.js

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { NewProducts } from "../components/NewProducts";
 import Products from "../components/Products";
 // import WallpaperSlider from "../components/WallpaperSlider";
 function Homepage() {
+  window.scrollBy(0, 0);
   return (
     <div>
       {/* <WallpaperSlider /> */}
+      <NewProducts />
       <Products />
     </div>
   );
